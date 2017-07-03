a36dcbbb01d6547b8f3b6000f3ba9bdddf9f81b5
mattapayne <paynmatt@gmail.com>
2017-07-03 14:35:44 -0700

    Adds v3 stripe javascript

--- lib/fake_stripe/stub_stripe_js.rb
+++ lib/fake_stripe/stub_stripe_js.rb
@@ -34,5 +34,27 @@ module FakeStripe
     def self.server_port
       @@stripe_js_port ||= FakeStripe::Utils.find_available_port
     end
+
+    get '/v3/' do
+      file_path = File.join(File.dirname(__FILE__), '/assets/v3.js')
+      mock_file_path = File.join(File.dirname(__FILE__), '/assets/v3-mock.js')
+
+      content_type 'text/javascript'
+      status 200
+      IO.read(file_path) + IO.read(mock_file_path)
+    end
+
+    def self.boot(port = FakeStripe::Utils.find_available_port)
+      instance = new
+      Capybara::Server.new(instance, port).tap { |server| server.boot }
+    end
+
+    def self.boot_once
+      @@stripe_js_server ||= FakeStripe::StubStripeJS.boot(self.server_port)
+    end
+
+    def self.server_port
+      @@stripe_js_port ||= FakeStripe::Utils.find_available_port
+    end
   end
 end
